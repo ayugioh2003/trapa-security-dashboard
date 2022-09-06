7b054609600899b788ceade96c8a62a1d01a052b
ayugioh2003 <ayugioh2003@gmail.com>
2022-09-06 11:47:51 +0800

    feat: vscode settings

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,9 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <main>
-      <h1 class="text-3xl font-bold underline text-red-500">
-    Hello world! (Home)
-  </h1>
+    <h1 class="text-3xl font-bold underline text-red-500">
+      Hello world! (Home)
+    </h1>
   </main>
 </template>
