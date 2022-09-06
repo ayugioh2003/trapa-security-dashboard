83d33360137b7d56e5cdaffa7fc1f8a687818f48
ayugioh2003 <ayugioh2003@gmail.com>
2022-09-06 11:42:34 +0800

    feat: tailwind init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+      <h1 class="text-3xl font-bold underline text-red-500">
+    Hello world! (Home)
+  </h1>
   </main>
 </template>
