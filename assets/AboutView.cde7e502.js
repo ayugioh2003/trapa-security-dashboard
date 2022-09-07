5fc9aaa6f9e5cbf02dc6b7e9a0eaa3f9696f7b0e
ayugioh2003 <ayugioh2003@users.noreply.github.com>
2022-09-07 14:17:31 +0000

    deploy: 1e61e1a55a27e2265781e899d300e77c9ea7ded3

---
+++ assets/AboutView.cde7e502.js
@@ -0,0 +1,1 @@
+import{_ as t,o as s,c as o,a as e}from"./index.a772410e.js";const _={},n={class:"about"},c=e("h1",null,"This is an about page",-1),a=e("h2",null,"This is an h2 element",-1),i=[c,a];function l(r,u){return s(),o("div",n,i)}const h=t(_,[["render",l]]);export{h as default};
